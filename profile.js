e149bc4a1a9e1fe3959c61d9725ea45ea284dac5
YUN MIN KYU <ymk1211@kaist.ac.kr>
2018-05-16 03:13:52 +0900

    profile: idol select

--- profile.js
+++ profile.js
@@ -21,6 +21,7 @@ firebase.auth().onAuthStateChanged(function(user) {
     uid = user.uid;
     console.log(uid);
     star_dict_init();
+    init_idols();
   } else {
     // User is signed out.
     console.log("singed out");
@@ -33,7 +34,6 @@ firebase.auth().onAuthStateChanged(function(user) {
 var database = firebase.database();
 var storage = firebase.storage();
 var storageRef = storage.ref();
-var databaseRef = database.ref();
 
 $( document ).ready(function() {
   initApp();
@@ -41,7 +41,7 @@ $( document ).ready(function() {
 
 function star_dict_init() {
     var query = database.ref("stars/"+uid);
-    console.log("star_dict_init");
+    // console.log("star_dict_init");
     query.once("value")
       .then(function(snapshot) {
         snapshot.forEach(function(childSnapshot) {
@@ -50,7 +50,7 @@ function star_dict_init() {
       });
     })
     .then(function() {
-      console.log(star_dict);
+      // console.log(star_dict);
       update_info();
     });
 }
@@ -60,16 +60,8 @@ function update_info() {
   renderStarVideos();
 }
 
-// var img = document.createElement("img");
-// img.src = "http://www.google.com/intl/en_com/images/logo_plain.png";
-
-// var src = document.getElementById("header");
-// src.appendChild(img);
-
-
-
 function renderStarVideos() {
-  console.log(Object.keys(star_dict));
+  // console.log(Object.keys(star_dict));
 
   Object.keys(star_dict).forEach(function(video) {
     // var img = document.createElement("img");
@@ -80,15 +72,17 @@ function renderStarVideos() {
   });
 }
 
+$('.main_page').on('click', function() {
+  update_idols();
+  window.location.href="sungduck.html";
+});
+
+var idols=[];
 var autocomplete = new SelectPure(".idols-select", {
   options: [
     {
       label: "레드벨벳",
       value: "레드벨벳",
-    },
-    {
-      label: "트와이스",
-      value: "트와이스",
     },
     {
       label: "BTS",
@@ -99,12 +93,17 @@ var autocomplete = new SelectPure(".idols-select", {
       value: "EXO",
     },
     {
+      label: "트와이스",
+      value: "트와이스",
+    },
+    {
       label: "Wanna One",
       value: "WannaOne",
     },
     {
       label: "여자친구",
       value: "girlfriend",
+      disabled: "true",
     },
     {
       label: "GOT7",
@@ -119,5 +118,48 @@ var autocomplete = new SelectPure(".idols-select", {
   multiple: true,
   autocomplete: true,
   icon: "fa fa-times",
-  onChange: value => { console.log(value); },
+  onChange: value => {
+    idols = value; 
+    console.log(value);
+  },
 });
+
+function update_idols() {
+  database.ref("idols/"+uid).set(idols);
+}
+
+function init_idols() {
+  var ref = database.ref("idols/"+uid);
+  ref.on('value', function(snapshot) {
+    if (snapshot.exists()) {
+      snapshot.forEach(function(childSnapshot) {
+        idols.push(childSnapshot.val());
+      });
+    }
+    else {
+      idols.push("트와이스");
+    }
+    update_labels();
+  });
+}
+
+
+var options=$('.select-pure__option');
+for (i=0; i<options.length; i++) {
+  if (["트와이스", "BTS"].indexOf(options[i].innerHTML) < 0) {
+    options[i].style.color = "#e4e4e4";
+    options[i].style.pointerEvents = "none";
+    options[i].style.cursor= "initial";
+  }
+}
+
+function update_labels() {
+  idols.forEach(idol=> {
+    for (i=0; i<options.length; i++) {
+      if (options[i].innerHTML == idol) {
+        options[i].click();
+        options[i].click();
+      }
+    }
+  });
+}
